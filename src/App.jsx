efc24d10d5e0dcd0f541bcdcb1e3a77aa222cf8e
GGvibes <grherzog@gmail.com>
2025-03-04 12:42:43 -0600

    dropdown menu styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import './App.css'
-import MainPage from './components/MainPage';
-import Dropdown from './components/Dropdown';
+import "./App.css";
+import MainPage from "./components/MainPage";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-
   useEffect(() => {
-    fetch ("http://localhost:5000/api/users")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-  },[]) 
+    fetch("http://localhost:5000/api/users")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <>
-    <Dropdown/>
-    <>
-    
-    </>
+      <Dropdown>
+        <div>
+          <div className="menu-item" >Log In</div>
+          <div className="menu-item">Sign Up</div>
+          <hr />
+          <div className="menu-item">Learn More</div>
+        </div>
+      </Dropdown>
+
       <Routes>
         <Route path="/" element={<MainPage></MainPage>}></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
